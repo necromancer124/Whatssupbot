071b10437d78636a3f22239ded921c201599d09d
Haladilnik <goddov@gmail.com>
2024-11-12 14:53:18 +0200

    the programaaaaaa

---
+++ SendMessage/Send.py
@@ -0,0 +1,4 @@
+import pywhatkit
+##i use pywhatkit to send a messge to a phone number of your choice and a time of your choice in the whatsapp web
+
+pywhatkit.sendwhatmsg(input("enter phon number:"),input("enter message"),int(input("at what hour")), int(input("enter at what minute")))
